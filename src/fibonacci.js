dd619c7ed76c4e1564630f202b957e45a04ab420
joshrhughes <josh@mtnpine.us>
2018-01-03 15:45:05 -0700

    solved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,3 +6,17 @@
      * @returns {Number} the nth element of the Fibonacci sequence.
      */
     // TODO: return definition of `fibonacci` function here
+
+function fibonacci (index) {
+    if(index < 0){
+        return 0;
+    }
+    else if(index <=1){
+        return 1;
+    }
+    else {
+        return fibonacci(index-1) + fibonacci(index-2);
+    }
+}
+
+module.exports = fibonacci;
